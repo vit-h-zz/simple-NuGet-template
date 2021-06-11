d386e41bca1d4bb1ee6c5b4a35f0a30540036b5c
Vitalii Honcharuk <vitalii@monsterlg.com>
2021-06-11 12:50:44 +0300

    add template

---
+++ SuperApp/SuperApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuperApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SuperApp!");
+        }
+    }
+}
